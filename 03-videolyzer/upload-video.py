cad8ebe3d4a432fa30a7fe2030bd65381048e7ef
sbreevoort <stefan.breevoort@gmail.com>
2020-03-22 22:26:45 +0100

    Setup serverless project for videolyzer

---
+++ 03-videolyzer/upload-video.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+
+import boto3
+import click
+
+
+@click.option('--profile', default=None, help="AWS profile")
+@click.argument('pathname', type=click.Path(exists=True))
+@click.argument('bucketname')
+@click.command()
+def upload_video(profile, pathname, bucketname):
+    """Upload <pathname> to <bucketname>"""
+
+    session_cfg = {}
+    if profile:
+        session_cfg['profile_name'] = profile
+
+        session = boto3.Session(**session_cfg)
+        s3 = session.resource('s3')
+        bucket = s3.Bucket(bucketname)
+        path = Path(pathname).expanduser().resolve()
+        bucket.upload_file(str(path), str(path.name))
+
+if __name__ == '__main__':
+    upload_video()
